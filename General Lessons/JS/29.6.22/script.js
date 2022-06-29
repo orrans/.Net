a2b89a8f54f883bf135b338c2220f12dc73e6197
orrans <orrans@users.noreply.github.com>
2022-06-29 19:51:29 +0300

    update git

---
+++ General Lessons/JS/29.6.22/script.js
@@ -0,0 +1,10 @@
+(function (window, document) {
+  const color = [
+    "rgb(255,0,0)",
+    "rgb(255,255,0)",
+    "rgb(0,255,0)",
+    "rgb(0,255,255)",
+    "rgb(0,0,255)",
+    "rgb(255,0,255)",
+  ];
+})(window, document);
